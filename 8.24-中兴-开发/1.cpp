9eac958d24b1cdfff2b293085ae2fc3d107e563a
ymshi <ymshi@mail.ustc.edu.cn>
2020-08-24 15:04:34 +0800

    Update 1.cpp

--- 8.24-中兴-开发/1.cpp
+++ 8.24-中兴-开发/1.cpp
@@ -5,11 +5,10 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    int vec[n];
-    int ans = 0;
+    int ans = 0, cur;
     for (int i = 0; i < n; i++) {
-        cin >> vec[i];
-        ans = max(ans, vec[i]);
+        cin >> cur;
+        ans = max(ans, cur);
     }
     cout << ans << endl;
     return 0;
